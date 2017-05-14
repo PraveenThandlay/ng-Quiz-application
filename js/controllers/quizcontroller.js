d297a2440fb8ed322ce076faec81d2a3def52c59
santoshthandlay <santoshthandlay@gmail.com>
2017-05-14 15:00:15 -0400

    inital app code

---
+++ js/controllers/quizcontroller.js
@@ -0,0 +1,12 @@
+(function(){
+	angular
+		   .module("turtleFacts")
+		   .controller("quizCtrl",QuizController);
+
+		   	QuizController.$inject = ['quizMetrics','DataService'];
+
+		   function QuizController(quizMetrics,DataService){
+		   		var vm =this;
+		   		vm.quizMetrics = quizMetrics;
+		   }
+})();
